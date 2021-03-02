1dcd191f68b87edb2dbb43d29301f4f08d383071
Bobber Colding-Poulsen <bkcp@Bobbers-MacBook-Pro.local>
2021-03-02 15:31:27 +0100

    opgave 6

--- Galaga/Game.cs
+++ Galaga/Game.cs
@@ -1,16 +1,78 @@
 using DIKUArcade;
 using DIKUArcade.Timers;
+using System.IO;
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+using System.Collections.Generic;
+using DIKUArcade.EventBus;
 
 namespace Galaga {
-    public class Game {
+    // NOTE: We implement the IGameEventProcessor interface!
+    public class Game : IGameEventProcessor<object> {
         private Window window;
         private GameTimer gameTimer;
+        private Player player;
+        private GameEventBus<object> eventBus;
 
         public Game() {
             window = new Window("Galaga", 500, 500);
             gameTimer = new GameTimer(30, 30);
+            player = new Player(
+                new DynamicShape(new Vec2F(0.45f, 0.1f), new Vec2F(0.1f, 0.1f)),
+                new Image(Path.Combine("Assets", "Images", "Player.png"))
+            );
+            eventBus = new GameEventBus<object>();
+            eventBus.InitializeEventBus(new List<GameEventType> { GameEventType.InputEvent });
+            window.RegisterEventBus(eventBus);
+            eventBus.Subscribe(GameEventType.InputEvent, this);
+            
         }
-        
+
+        public void KeyPress(string key) {
+            // TODO: switch on key string and set the player's move direction
+            switch (key) {
+                case "KEY_LEFT":
+                    player.SetMoveLeft(true);
+                    break;
+                case "KEY_RIGHT":
+                    player.SetMoveRight(true);
+                    break;
+                case "KEY_ESCAPE":
+                    window.CloseWindow();
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void KeyRelease(string key) {
+            // TODO: switch on key string and disable the player's move direction
+            // TODO: Close window if escape is pressed
+            switch (key) {
+                case "KEY_LEFT":
+                    player.SetMoveLeft(false);
+                    break;
+                case "KEY_RIGHT":
+                    player.SetMoveRight(false);
+                    break;
+                default:
+                    break;
+            }
+        }
+        public void ProcessEvent(GameEventType type, GameEvent<object> gameEvent) {
+            switch (gameEvent.Parameter1) {
+                case "KEY_PRESS":
+                    KeyPress(gameEvent.Message);
+                    break;
+                case "KEY_RELEASE":
+                    KeyRelease(gameEvent.Message);
+                    break;
+                default:
+                    break;
+            }
+        } 
+
+
         public void Run() {
             while(window.IsRunning()) {
                 gameTimer.MeasureTime();
@@ -19,12 +81,16 @@ namespace Galaga {
                     window.PollEvents();
 
                     // update game logic here...
+                    eventBus.ProcessEvents();
+                    player.Move();
+                
                 }
             
                 if (gameTimer.ShouldRender()) {
                     window.Clear();
 
                     // render game entities here...
+                    this.player.Render();
 
                     window.SwapBuffers();
                 }
---
+++ Galaga/Player.cs
@@ -0,0 +1,52 @@
+using DIKUArcade.Entities;
+using DIKUArcade.Graphics;
+using DIKUArcade.Math;
+
+namespace Galaga {
+    public class Player {
+        private Entity entity;
+        private DynamicShape shape;
+
+        // TODO: Add private fields
+        private float moveLeft = 0.0f;
+        private float moveRight = 0.0f;
+        const float MOVEMENT_SPEED = 0.01f;
+        
+        public Player(DynamicShape shape, IBaseImage image) {
+            entity = new Entity(shape, image);
+            this.shape = shape;
+        }
+        
+        public void Render() {
+            entity.RenderEntity();
+        }
+
+        // move the shape and guard against the window borders
+        public void Move() { // FIX BOUNDARY
+            if (this.shape.Position.X >= 0.94f) {
+                this.shape.Position.X = 0.94f;
+            }
+            if (this.shape.Position.X < 0f) {
+                this.shape.Position.X = 0f;
+            }
+            this.shape.Move();
+        }
+
+        // set moveLeft appropriately and call UpdateMovement() / UpdateDirection
+        public void SetMoveLeft(bool val) {
+            if (val) { this.moveLeft -= MOVEMENT_SPEED; }
+            UpdateDirection();
+        }
+
+        // set moveRight appropriately and call UpdateMovement() / UpdateDirection
+        public void SetMoveRight(bool val) {
+            if (val) { this.moveRight += MOVEMENT_SPEED; }
+            UpdateDirection();
+        }
+        private void UpdateDirection() {
+            this.shape.MoveX(moveRight + moveLeft);
+            this.moveLeft = 0.0f;
+            this.moveRight = 0.0f;
+        }
+    }
+}
