194ade4b96f08833245fc9c762125dc6b5eb11a7
Bobber Colding-Poulsen <bkcp@protonmail.com>
2021-06-01 12:23:12 +0200

    fixed TestPowerUp
    Co-authored-by: Oscar Kofod <okofod@icloud.com>

--- BreakoutTests/EntityTests/TestPowerUp.cs
+++ BreakoutTests/EntityTests/TestPowerUp.cs
@@ -2,53 +2,49 @@ using NUnit.Framework;
 using DIKUArcade.Events;
 using Breakout.BreakoutStates; 
 using System.Collections.Generic;
+using Breakout;
 
-namespace Breakout {
+namespace BreakoutTests {
 
     public class PowerUpTests {
-    private Player player;
-    private StateMachine stateMachine;
-    private GameEventBus eventBus;
+        private Player player;
+        private StateMachine stateMachine;
+        private GameEventBus eventBus;
 
-    [SetUp]
-    public void InitiateGame() {
-        DIKUArcade.GUI.Window.CreateOpenGLContext();   
-        player = new Player ();
-        stateMachine = new StateMachine();
-        eventBus = new GameEventBus();
-        eventBus.InitializeEventBus(new List<GameEventType> {
-            GameEventType.GameStateEvent, GameEventType.PlayerEvent
-        });
-        eventBus.Subscribe(GameEventType.GameStateEvent, stateMachine);
-        eventBus.Subscribe(GameEventType.PlayerEvent, player);
+        [SetUp]
+        public void InitiateGame() {
+            DIKUArcade.GUI.Window.CreateOpenGLContext();   
+            player = new Player ();
+            stateMachine = new StateMachine();
+            eventBus = new GameEventBus();
+            eventBus.InitializeEventBus(new List<GameEventType> {
+                GameEventType.GameStateEvent, GameEventType.PlayerEvent
+            });
+            eventBus.Subscribe(GameEventType.GameStateEvent, stateMachine);
+            eventBus.Subscribe(GameEventType.PlayerEvent, player);
 
-        eventBus.RegisterEvent(new GameEvent {
+            eventBus.RegisterEvent(new GameEvent {
                 EventType  = GameEventType.GameStateEvent,
                 Message    = "GameRunning"
             });
-    }       
+        }
 
+        [Test]
 
-    [Test]
+        public void TestLifePickUp() {
+            Status.GetStatus().Reset();
+            // Lives should be 3.
+            Assert.AreEqual(3, Status.GetStatus().lives);
+            Status.GetStatus().ExtraLife();
+            // Lives should be 4.
+            Assert.AreEqual(4, Status.GetStatus().lives);}
 
-    public void TestLifePickUp() {
-        Status.GetStatus().Reset();
-        // Lives should be 3.
-        Assert.AreEqual(3, Status.GetStatus().lives);
-        Status.GetStatus().ExtraLife();
-        // Lives should be 4.
-        Assert.AreEqual(4, Status.GetStatus().lives);}
-
-    [Test]
-    public void TestWidePowerUp() {
-        Assert.AreEqual(0.15, player.mutableXtent, 0.1);
-        //Player size after the powerup.
-        player.WidenPlayer();
-        Assert.AreEqual(0.225,player.mutableXtent, 0.1);
-
+        [Test]
+        public void TestWidePowerUp() {
+            Assert.AreEqual(0.15, player.mutableXtent, 0.1);
+            //Player size after the powerup.
+            player.WidenPlayer();
+            Assert.AreEqual(0.225,player.mutableXtent, 0.1);
+        }
     }
-
-    }
-
-
 }
